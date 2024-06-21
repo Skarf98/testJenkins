bb201f63c0d1aec5eb97a9e9e43be7ecbbd50447
Skarf98 <161575707+Skarf98@users.noreply.github.com>
2024-06-21 12:59:45 +0200

    Update new 1.java

--- new 1.java
+++ new 1.java
@@ -8,7 +8,7 @@ public class CasePDFGenerate{
     @AuraEnabled(cacheable=true) 
         public static Case tCase(Id recordId) { 
             
-            Case cs = [SELECT Id,CaseNumber FROM Case WHERE Id=:recordId];
-            return cs;
+            Case c = [SELECT Id,CaseNumber FROM Case WHERE Id=:recordId];
+            return c;
        } 
 }
